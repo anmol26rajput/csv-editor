a5a57de7808321edf016c1ed9e7912470e096552
Anmol <anmol26rajput@gmail.com>
2026-02-13 03:54:39 +0400

    fixes and layout changes

--- editor/admin.py
+++ editor/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import CSVFile
+from .models import Document
 
-@admin.register(CSVFile)
-class CSVFileAdmin(admin.ModelAdmin):
-    list_display = ['name', 'uploaded_at', 'size', 'row_count', 'column_count']
-    list_filter = ['uploaded_at']
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'file_type', 'uploaded_at', 'size', 'row_count', 'column_count']
+    list_filter = ['uploaded_at', 'file_type']
     search_fields = ['name']
-
--- editor/models.py
+++ editor/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from django.utils import timezone
 import os
 
-class CSVFile(models.Model):
+class Document(models.Model):
+    FILE_TYPES = [
+        ('csv', 'CSV'),
+        ('pdf', 'PDF'),
+        ('docx', 'Word Document'),
+        ('pptx', 'PowerPoint'),
+        ('other', 'Other')
+    ]
+
     name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='csv_files/')
+    file = models.FileField(upload_to='documents/')
+    file_type = models.CharField(max_length=10, choices=FILE_TYPES, default='csv')
     uploaded_at = models.DateTimeField(default=timezone.now)
     size = models.IntegerField(default=0)
-    row_count = models.IntegerField(default=0)
-    column_count = models.IntegerField(default=0)
+    
+    # CSV specific fields (nullable now)
+    row_count = models.IntegerField(null=True, blank=True)
+    column_count = models.IntegerField(null=True, blank=True)
     
     class Meta:
         ordering = ['-uploaded_at']
@@ -16,6 +27,21 @@ class CSVFile(models.Model):
     def __str__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        # Auto-detect file type based on extension
+        ext = os.path.splitext(self.file.name)[1].lower()
+        if ext == '.csv':
+            self.file_type = 'csv'
+        elif ext == '.pdf':
+            self.file_type = 'pdf'
+        elif ext in ['.docx', '.doc']:
+            self.file_type = 'docx'
+        elif ext in ['.pptx', '.ppt']:
+            self.file_type = 'pptx'
+        else:
+            self.file_type = 'other'
+        super().save(*args, **kwargs)
+
     def delete(self, *args, **kwargs):
         """Delete the file when the model is deleted"""
         if self.file:
