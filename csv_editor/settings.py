41151d744a03f43ae6b97251a3aa092a2838ee32
Anmol <anmol26rajput@gmail.com>
2026-02-18 17:34:41 +0400

    x frame options

--- csv_editor/settings.py
+++ csv_editor/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 X_FRAME_OPTIONS = 'ALLOWALL'
