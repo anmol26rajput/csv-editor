88562687abde4c08992e6c4d40feecb523390255
Anmol <anmol26rajput@gmail.com>
2026-02-18 17:26:58 +0400

    settings change

--- csv_editor/settings.py
+++ csv_editor/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -44,6 +46,7 @@ MIDDLEWARE = [
 ]
 
 X_FRAME_OPTIONS = 'ALLOWALL'
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'csv_editor.urls'
 
