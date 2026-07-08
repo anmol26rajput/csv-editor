1aad42960828b2136990cfa2fafdd56c683da399
Anmol <anmol26rajput@gmail.com>
2026-07-08 16:03:01 +0400

    Remove the main HTML template for the OmniFile editor, consolidating the codebase and streamlining the project structure.

--- editor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Document
-
-@admin.register(Document)
-class DocumentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'file_type', 'uploaded_at', 'size', 'row_count', 'column_count']
-    list_filter = ['uploaded_at', 'file_type']
-    search_fields = ['name']
